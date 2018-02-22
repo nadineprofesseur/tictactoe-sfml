cb0ecef47affb2707b91abdfcd67bea9f702e7d8
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-02-22 10:42:07 -0500

    Un client se connecte au serveur

--- TicTacToeClient/src/App.cpp
+++ TicTacToeClient/src/App.cpp
@@ -1,9 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include "PageGrilleDeJeu.h"
+// https://www.sfml-dev.org/tutorials/2.4/network-socket.php
+#include <SFML/Network.hpp>
+#include <iostream>
 
 using namespace sf;
 int main()
 {
+	static int PORT = 2000;
+	static IpAddress HOTE = "127.0.0.1";
+	TcpSocket connexion;
+	Socket::Status status = connexion.connect(HOTE, PORT);
+	if (status != sf::Socket::Done)
+	{
+	}
+	/*
     RenderWindow * fenetre = new RenderWindow(VideoMode(800, 800), "SFML !");
     PageGrilleDeJeu * pageGrilleDeJeu = new PageGrilleDeJeu(fenetre);
     while (fenetre->isOpen())
@@ -19,7 +30,7 @@ int main()
         pageGrilleDeJeu->afficherNomO("Nadine");
         pageGrilleDeJeu->afficherNomX("Sophie");
         fenetre->display();
-    }
+    }*/
     return 0;
 }
 
--- TicTacToeServeur/src/AppServeur.cpp
+++ TicTacToeServeur/src/AppServeur.cpp
@@ -1,4 +1,21 @@
+
+// https://www.sfml-dev.org/tutorials/2.4/network-socket.php
+#include <SFML/Network.hpp>
+#include <iostream>
+
+using namespace sf;
 int main()
 {
+	static int PORT = 2000;
+	TcpListener ecouteur;
+	TcpSocket connexion;
 
+	if (ecouteur.listen(PORT) != Socket::Done)
+	{
+	}
+	if (ecouteur.accept(connexion) != Socket::Done)
+	{
+	}
+
+	std::cout << "Connexion acceptee " << std::endl;
 }
